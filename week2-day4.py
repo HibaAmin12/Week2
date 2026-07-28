7e79e9dc90df0b3f424e235ce5b3fcabb6628f4c
HibaAmin12 <mscsf24m005@pucit.edu.pk>
2026-07-28 20:21:27 +0500

    uncommet code of week2-day4 file

--- week2-day4.py
+++ week2-day4.py
@@ -1,301 +1,301 @@
-##SEQUENTIAL EXECUTION
+#SEQUENTIAL EXECUTION
 
-# import time
+import time
 
-# def task(name):
-#     print(f"{name} started")
-#     time.sleep(2)
-#     print(f"{name} finished")
-
-
-# start = time.time()
-
-# task("Task 1")
-# task("Task 2")
-# task("Task 3")
-
-# end = time.time()
-
-# print(f"Total time: {end - start:.2f} seconds")
-
-## Concurrency using Threading
-# import time
-# from concurrent.futures import ThreadPoolExecutor
+def task(name):
+    print(f"{name} started")
+    time.sleep(2)
+    print(f"{name} finished")
 
 
-# def task(name):
-#     print(f"{name} started")
-#     time.sleep(2)
-#     print(f"{name} finished")
+start = time.time()
+
+task("Task 1")
+task("Task 2")
+task("Task 3")
+
+end = time.time()
+
+print(f"Total time: {end - start:.2f} seconds")
+
+# Concurrency using Threading
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 
-# start = time.time()
-
-# with ThreadPoolExecutor(max_workers=2) as executor:
-#     executor.map(
-#         task,
-#         ["Task 1", "Task 2", "Task 3"]
-#     )
+def task(name):
+    print(f"{name} started")
+    time.sleep(2)
+    print(f"{name} finished")
 
 
-# end = time.time()
+start = time.time()
 
-# print(f"Total time: {end - start:.2f} seconds")
-
-
-## True Parallelism using Multiprocessing
-
-# from concurrent.futures import ProcessPoolExecutor
-# import time
+with ThreadPoolExecutor(max_workers=2) as executor:
+    executor.map(
+        task,
+        ["Task 1", "Task 2", "Task 3"]
+    )
 
 
-# def cpu_task(n):
-#     total = 0
+end = time.time()
 
-#     for i in range(n):
-#         total += i * i
-
-#     return total
+print(f"Total time: {end - start:.2f} seconds")
 
 
-# numbers = [20_000_000, 20_000_000, 20_000_000]
+# True Parallelism using Multiprocessing
+
+from concurrent.futures import ProcessPoolExecutor
+import time
 
 
-# start = time.time()
+def cpu_task(n):
+    total = 0
 
-# with ProcessPoolExecutor(max_workers=3) as executor:
-#     results = executor.map(cpu_task, numbers)
+    for i in range(n):
+        total += i * i
 
-
-# end = time.time()
-
-# print(list(results))
-# print(f"Time: {end-start:.2f} seconds")
-
-##ASYNC IO
-# import asyncio
-# import time
+    return total
 
 
-# async def task(name):
-#     print(f"{name} started")
-
-#     await asyncio.sleep(2)
-
-#     print(f"{name} finished")
+numbers = [20_000_000, 20_000_000, 20_000_000]
 
 
-# async def main():
+start = time.time()
 
-#     start = time.time()
-
-#     await asyncio.gather(
-#         task("Task 1"),
-#         task("Task 2"),
-#         task("Task 3")
-#     )
-
-#     end = time.time()
-
-#     print(f"Total time: {end-start:.2f} seconds")
+with ProcessPoolExecutor(max_workers=3) as executor:
+    results = executor.map(cpu_task, numbers)
 
 
-# asyncio.run(main())
+end = time.time()
+
+print(list(results))
+print(f"Time: {end-start:.2f} seconds")
+
+#ASYNC IO
+import asyncio
+import time
 
 
-# import time
-# from concurrent.futures import ThreadPoolExecutor
+async def task(name):
+    print(f"{name} started")
+
+    await asyncio.sleep(2)
+
+    print(f"{name} finished")
 
 
-# def download_file(file):
-#     start = time.time()
+async def main():
 
-#     print(f"Downloading {file}...")
+    start = time.time()
 
-#     # Simulating file download (I/O waiting)
-#     time.sleep(2)
+    await asyncio.gather(
+        task("Task 1"),
+        task("Task 2"),
+        task("Task 3")
+    )
 
-#     print(f"{file} downloaded.")
+    end = time.time()
 
-#     end = time.time()
-
-#     print(f"Time taken for {file}: {end - start:.2f} seconds")
-
-
-# files = [
-#     "file1.txt",
-#     "file2.txt",
-#     "file3.txt",
-#     "file4.txt"
-# ]
+    print(f"Total time: {end-start:.2f} seconds")
 
 
-# # Overall execution start time
-# overall_start = time.time()
+asyncio.run(main())
 
 
-# with ThreadPoolExecutor(max_workers=3) as executor:
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+def download_file(file):
+    start = time.time()
+
+    print(f"Downloading {file}...")
+
+    # Simulating file download (I/O waiting)
+    time.sleep(2)
+
+    print(f"{file} downloaded.")
+
+    end = time.time()
+
+    print(f"Time taken for {file}: {end - start:.2f} seconds")
+
+
+files = [
+    "file1.txt",
+    "file2.txt",
+    "file3.txt",
+    "file4.txt"
+]
+
+
+# Overall execution start time
+overall_start = time.time()
+
+
+with ThreadPoolExecutor(max_workers=3) as executor:
     
-#     # Run download_file concurrently on multiple threads
-#     list(executor.map(download_file, files))
+    # Run download_file concurrently on multiple threads
+    list(executor.map(download_file, files))
 
 
-# # Overall execution end time
-# overall_end = time.time()
+# Overall execution end time
+overall_end = time.time()
 
 
-# print("----------------------------")
-# print(f"All files downloaded.")
-# print(f"Total execution time: {overall_end - overall_start:.2f} seconds")
+print("----------------------------")
+print(f"All files downloaded.")
+print(f"Total execution time: {overall_end - overall_start:.2f} seconds")
 
 
-# import time
-# from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import time
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 
-# def cpu_task(n):
-#     total = 0
+def cpu_task(n):
+    total = 0
 
-#     for i in range(n):
-#         total += i * i
+    for i in range(n):
+        total += i * i
 
-#     return total
+    return total
 
 
-# numbers = [
-#     20_000_000,
-#     20_000_000,
-#     20_000_000
-# ]
+numbers = [
+    20_000_000,
+    20_000_000,
+    20_000_000
+]
 
 
-# # -------------------------------
-# # 1. Sequential Execution
-# # -------------------------------
+# -------------------------------
+# 1. Sequential Execution
+# -------------------------------
 
-# start = time.time()
+start = time.time()
 
-# for number in numbers:
-#     cpu_task(number)
+for number in numbers:
+    cpu_task(number)
 
-# end = time.time()
+end = time.time()
 
-# print(f"Sequential time: {end-start:.2f} seconds")
+print(f"Sequential time: {end-start:.2f} seconds")
 
 
 
-# # -------------------------------
-# # 2. ThreadPoolExecutor
-# # -------------------------------
+# -------------------------------
+# 2. ThreadPoolExecutor
+# -------------------------------
 
-# start = time.time()
+start = time.time()
 
-# with ThreadPoolExecutor(max_workers=3) as executor:
-#     list(executor.map(cpu_task, numbers))
+with ThreadPoolExecutor(max_workers=3) as executor:
+    list(executor.map(cpu_task, numbers))
 
-# end = time.time()
+end = time.time()
 
-# print(f"Threading time: {end-start:.2f} seconds")
+print(f"Threading time: {end-start:.2f} seconds")
 
 
 
-# # -------------------------------
-# # 3. ProcessPoolExecutor
-# # -------------------------------
+# -------------------------------
+# 3. ProcessPoolExecutor
+# -------------------------------
 
-# start = time.time()
+start = time.time()
 
-# with ProcessPoolExecutor(max_workers=3) as executor:
-#     list(executor.map(cpu_task, numbers))
+with ProcessPoolExecutor(max_workers=3) as executor:
+    list(executor.map(cpu_task, numbers))
 
-# end = time.time()
+end = time.time()
 
-# print(f"Multiprocessing time: {end-start:.2f} seconds")
+print(f"Multiprocessing time: {end-start:.2f} seconds")
 
 
-# import time
-# from concurrent.futures import ThreadPoolExecutor
+import time
+from concurrent.futures import ThreadPoolExecutor
 
-# def download(file):
-#     print(f"Downloading {file}...")
-#     time.sleep(2)      # Simulating network waiting
-#     print(f"{file} downloaded")
+def download(file):
+    print(f"Downloading {file}...")
+    time.sleep(2)      # Simulating network waiting
+    print(f"{file} downloaded")
 
-# files = ["A", "B", "C", "D"]
+files = ["A", "B", "C", "D"]
 
-# # Sequential
-# start = time.time()
+# Sequential
+start = time.time()
 
-# for file in files:
-#     download(file)
+for file in files:
+    download(file)
 
-# end = time.time()
+end = time.time()
 
-# print(f"\nSequential Time: {end-start:.2f} seconds")
+print(f"\nSequential Time: {end-start:.2f} seconds")
 
 
-# # Threading
-# start = time.time()
+# Threading
+start = time.time()
 
-# with ThreadPoolExecutor(max_workers=4) as executor:
-#     list(executor.map(download, files))
+with ThreadPoolExecutor(max_workers=4) as executor:
+    list(executor.map(download, files))
 
-# end = time.time()
+end = time.time()
 
-# print(f"Threading Time: {end-start:.2f} seconds")
+print(f"Threading Time: {end-start:.2f} seconds")
 
 
-# import asyncio
+import asyncio
 
-# async def work(name):
-#     print(f"{name} Start")
-#     await asyncio.sleep(2)
-#     print(f"{name} End")
+async def work(name):
+    print(f"{name} Start")
+    await asyncio.sleep(2)
+    print(f"{name} End")
 
-# async def main():
-#     await asyncio.gather(
-#         work("A"),
-#         work("B"),
-#         work("C")
-#     )
+async def main():
+    await asyncio.gather(
+        work("A"),
+        work("B"),
+        work("C")
+    )
 
-# asyncio.run(main())
+asyncio.run(main())
 
-# import requests
+import requests
 
-# response = requests.get("https://api.github.com")
+response = requests.get("https://api.github.com")
 
-# print(response)
-# print(response.status_code)
+print(response)
+print(response.status_code)
 
-# print(response.headers)
+print(response.headers)
 
-# print(response.text)
+print(response.text)
 
-# print(response.json())
+print(response.json())
 
-# import requests
+import requests
 
-# response = requests.get("https://api.github.com")
+response = requests.get("https://api.github.com")
 
-# data = response.json()
+data = response.json()
 
-# print(data["current_user_url"])
+print(data["current_user_url"])
 
 
-# import requests
+import requests
 
-# response = requests.get("https://api.github.com")
+response = requests.get("https://api.github.com")
 
-# if response.status_code == 200:
+if response.status_code == 200:
 
-#     data = response.json()
+    data = response.json()
 
-#     print(data["current_user_url"])
+    print(data["current_user_url"])
 
-# else:
+else:
 
-#     print("Request Failed")
+    print("Request Failed")
 
 import requests
 
